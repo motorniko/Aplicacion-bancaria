fc9d8652ede76a0d3c0ffd618862f3002cb113dc
motorniko <albertolagos59@gmail.com>
2025-04-07 23:32:13 -0400

    agregando primeros cambios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("BIENVENIDO A BANCO - HIGTH CLASS");
+
+        System.out.println("1. Consultar saldo");
+        System.out.println("2. Retirar dinero");
+        System.out.println("3.Depositar dinero");
+        System.out.println("4. Salir");
+
+        System.out.println("Ingrese la opción deseada: ");
+        Scanner scanner = new Scanner(System.in);
+
+
+
     }
 }
