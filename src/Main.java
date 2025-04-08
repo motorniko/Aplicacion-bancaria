382d5ef8a8ed4fb159e35c40eb7369a50c3c07f5
motorniko <albertolagos59@gmail.com>
2025-04-07 23:50:52 -0400

    agregando menu

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,22 @@ public class Main {
 
         System.out.println("Ingrese la opción deseada: ");
         Scanner scanner = new Scanner(System.in);
+        int opcion = scanner.nextInt();
 
+        int saldo = (int) 2000;
+
+        if (opcion == 1){
+            System.out.println("Saldo disponible1: " + saldo);
+        } else if (opcion == 2) {
+            System.out.println("Ingrese la cantidad a retirar: ");
+            int Retiro = scanner.nextInt();
+            if (Retiro > saldo) {
+                System.out.println("Saldo insuficiente.");
+            } else {
+                int saldoDisp = saldo-Retiro    ;
+                System.out.println("Retiro exitoso. saldo disponiblel: " + saldoDisp);
+            }
+        }
 
 
     }
