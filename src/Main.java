87c9b602f9eafa3cf6978f31cf0dc221b5ab7027
motorniko <albertolagos59@gmail.com>
2025-04-08 00:10:45 -0400

    agregando funcionalidad a los menus

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int saldo = (int) 1599.99;
+
         System.out.println("BIENVENIDO A BANCO - HIGTH CLASS");
+
+        System.out.println("NOMBRE DEL CLIENTE: TONY STARK ");
+        System.out.println("TIPO DE CUENTA: CORRIENTE");
+        System.out.println("SALDO DISPONIBLE:" + saldo);
 
         System.out.println("1. Consultar saldo");
         System.out.println("2. Retirar dinero");
@@ -12,8 +18,6 @@ public class Main {
         System.out.println("Ingrese la opción deseada: ");
         Scanner scanner = new Scanner(System.in);
         int opcion = scanner.nextInt();
-
-        int saldo = (int) 2000;
 
         if (opcion == 1){
             System.out.println("Saldo disponible1: " + saldo);
@@ -26,8 +30,15 @@ public class Main {
                 int saldoDisp = saldo-Retiro    ;
                 System.out.println("Retiro exitoso. saldo disponiblel: " + saldoDisp);
             }
+        } else if (opcion == 3) {
+            System.out.println("Ingrese la cantidad a depositar: ");
+            int Deposito = scanner.nextInt();
+            int saldoDisp = saldo + Deposito;
+            System.out.println("Deposito exitoso. saldo disponible: " + saldoDisp);
+        } else if (opcion == 4) {
+            System.out.println("Gracias por utilizar BANCO - HIGTH CLASS.");
+        } else {
+            System.out.println("Opción no válida.");
         }
-
-
     }
 }
